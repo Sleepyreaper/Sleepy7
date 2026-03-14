bd3eb7d29dc6aaa6f70d8e6a5e0f586ee3aa82a9
Brad Allen <sleepyreaper@hotmail.com>
2026-03-14 13:21:35 -0400

    feat: team improvements: Sleepy7/Sources/Shared/Services/HapticManager.swift

---
+++ Sleepy7/Sources/Shared/Services/HapticManager.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+protocol HapticProviding: AnyObject {
+    func impactLight()
+    func impactMedium()
+    func notifySuccess()
+    func notifyWarning()
+    func notifyError()
+}
+
+final class HapticManager: HapticProviding {
+    func impactLight() {
+        UIImpactFeedbackGenerator(style: .light).impactOccurred()
+    }
+
+    func impactMedium() {
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+    }
+
+    func notifySuccess() {
+        UINotificationFeedbackGenerator().notificationOccurred(.success)
+    }
+
+    func notifyWarning() {
+        UINotificationFeedbackGenerator().notificationOccurred(.warning)
+    }
+
+    func notifyError() {
+        UINotificationFeedbackGenerator().notificationOccurred(.error)
+    }
+}
