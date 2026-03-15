a15192c323df4670b9b1db88fceaf6f16ffb0e05
Brad Allen <sleepyreaper@hotmail.com>
2026-03-14 20:05:19 -0400

    feat: team improvements: Sleepy7/App/Sleepy7App.swift

--- Sleepy7/App/Sleepy7App.swift
+++ Sleepy7/App/Sleepy7App.swift
@@ -1,8 +1,15 @@
 import SwiftUI
+import FirebaseCore
+import FirebaseCrashlytics
+import FirebasePerformance
 
 @main
 struct Sleepy7App: App {
     @StateObject private var engine = GameEngine()
+
+    init() {
+        FirebaseBootstrap.configure()
+    }
 
     var body: some Scene {
         WindowGroup {
