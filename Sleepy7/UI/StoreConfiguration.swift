51071a57ff68c9360e234cc1600808602d5594e4
Brad Allen <sleepyreaper@hotmail.com>
2026-03-14 20:05:10 -0400

    feat: team improvements: Sleepy7/UI/StoreConfiguration.swift

---
+++ Sleepy7/UI/StoreConfiguration.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+enum StoreConfiguration {
+    static let adFreeProductID = "ad_free_upgrade"
+    static let premiumThemesProductID = "premium_themes_pack"
+
+    static let productIDs: [String] = [
+        adFreeProductID,
+        premiumThemesProductID
+    ]
+}
